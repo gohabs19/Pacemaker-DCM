de2ef659fa928635a0e9aadf07bb1cadb1f9ba99
gohabs19 <57967821+gohabs19@users.noreply.github.com>
2019-11-28 21:03:44 -0500

    Create windows.py

---
+++ windows.py
@@ -0,0 +1,35 @@
+#A file filled with classes relating to each of the windows that will be needed. The windows themselves, the text and
+#the buttons that call the functionality that will be needed. The functionality for each of these windows will be held
+#in a seperate file.
+from tkinter import *
+import windowsFunctionality#To be able to use the methods contained within the windowsFunctionality classes
+
+class windowsStart:
+    def welcomeWindow(self,root):
+        print('Starting Welcome Window')#Print to bottom for debugging
+        root.title('Pacemaker DCM')#Window name
+        welcomeLabel = Label(root, text="Welcome to the pacemaker DCM! Please enter your login info!")# Welcome message
+        welcomeLabel.grid(row=1,sticky=N)# Assigns label to border
+        continueToLoginButton=Button(root, text='Continue to Login', fg='Black', command=lambda: windowsLogin.loginWindow(self,root))
+        continueToLoginButton.grid(row=2,sticky=S)
+
+class windowsLogin:
+    def loginWindow(self, root):
+        root.destroy()#Destroy the starting window and its mainloop
+        print('Opening Login Window')
+        lWindow=Tk()#Create a window to hold the login
+        lWindow.title('Login')#Window name
+        loginWindowsFunctionality=windowsFunctionality.loginAndRegistrationWindowsFunctionality().loginWindowsFunctionality()
+        loginWindowsFunctionality.fillLoginWindow(lWindow)#Call the fillLoginWindow method from the loginWindowsFunctionality
+        #class.
+        lWindow.mainloop()
+
+class windowsRegister:
+    def registerWindow(self, root):
+        root.destroy()
+        print('Opening Register Window')
+        rWindow=Tk()
+        rWindow.title('Register')
+        registrationWindowsFunctionality = windowsFunctionality.loginAndRegistrationWindowsFunctionality().registrationWindowsFunctionality()
+        registrationWindowsFunctionality.fillRegistrationWindow(rWindow)  # Call the fillLoginWindow method from the loginWindowsFunctionality
+        rWindow.mainloop()
